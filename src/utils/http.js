ee2926d6634d9cfa1c0a0c12efeaafa3d0477b06
Antoine Dray <antoine.dray@epita.fr>
2020-12-13 19:05:29 +0100

    feat: Update global axios config

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,10 +3,5 @@ import axios from 'axios'
 export const HTTP = axios.create({
   baseURL: 'http://localhost/api/v2/',
   headers: {
-    //'Accept': 'application/json',
-    //'Access-Control-Allow-Origin': '*',
-    //'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
-    //'Access-Control-Allow-Headers': '*'
-  },
-  crossDomain: true
+  }
 })
